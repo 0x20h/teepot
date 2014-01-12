49a23bfd31decdeadaf1873f814580fe2261b6b4
0x20h <kohj@informatik.uni-marburg.de>
2014-01-12 21:53:01 +0100

    s#^I#  #g

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 var prg = require('commander'),
-		fs = require('fs'),
-		log = require('winston'),
-		path = require('path')
+    fs = require('fs'),
+    log = require('winston'),
+    path = require('path')
 
 prg
-	.version('0.0.1')
-	.option('-c, --config [FILE]', 'configuration file', 
-			[__dirname, 'config.js'].join('/'))
-	.option('--host [HOST]', 'Redis server')
-	.option('--port [N]', 'Redis port')
-	.option('--worker [N]', 'number of workers to start')
-	.option('--command [CMD]', 'path to the script that starts workers')
-	.option('-v, --verbosity [LEVEL]', 'error, info, debug', 'info')
-	.parse(process.argv)
+  .version('0.0.1')
+  .option('-c, --config [FILE]', 'configuration file', 
+      [__dirname, 'config.js'].join('/'))
+  .option('--host [HOST]', 'Redis server')
+  .option('--port [N]', 'Redis port')
+  .option('--worker [N]', 'number of workers to start')
+  .option('--command [CMD]', 'path to the script that starts workers')
+  .option('-v, --verbosity [LEVEL]', 'error, info, debug', 'info')
+  .parse(process.argv)
 
 var configfile = path.resolve(prg.config)
 
 try {
-	var config = require(configfile)
+  var config = require(configfile)
 } catch (e) {
-	console.error("unable to load configuation from " + configfile)
-	process.exit(1)
+  console.error("unable to load configuation from " + configfile)
+  process.exit(1)
 }
 
 // log config
 log
-	.remove(log.transports.Console)
-	.add(log.transports.Console, { 
-		level: prg.verbosity,
-		colorize: true,
-		timestamp: true
-	})
+  .remove(log.transports.Console)
+  .add(log.transports.Console, { 
+    level: prg.verbosity,
+    colorize: true,
+    timestamp: true
+  })
 
-var	teepot = require('./lib')
+var  teepot = require('./lib')
 var supervisor = teepot.supervisor(config)
 // set reference to supervisor
 teepot.api.set('supervisor', supervisor)
